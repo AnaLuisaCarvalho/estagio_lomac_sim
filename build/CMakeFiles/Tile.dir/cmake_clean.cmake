f46841c20b8b5d637726d4049550d1100a50b782
Ana Luisa Carvalho (MSc 2017 ATLAS-LIP) <aluisa@pauli02.ncg.ingrid.pt>
2020-03-14 20:16:48 +0000

    Adding src and inc files with the same structure as in geant example B1. Just changed the names. It compiles

---
+++ build/CMakeFiles/Tile.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tile"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Tile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TileFCC.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TileFCC.dir/TileFCCmain.cc.o"
+  "CMakeFiles/TileFCC.dir/src/TileFCCActionInitialization.cc.o"
+  "CMakeFiles/TileFCC.dir/src/TileFCCDetectorConstruction.cc.o"
+  "CMakeFiles/TileFCC.dir/src/TileFCCEventAction.cc.o"
+  "CMakeFiles/TileFCC.dir/src/TileFCCPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/TileFCC.dir/src/TileFCCRunAction.cc.o"
+  "CMakeFiles/TileFCC.dir/src/TileFCCSteppingAction.cc.o"
+  "TileFCC.pdb"
+  "TileFCC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TileFCC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
